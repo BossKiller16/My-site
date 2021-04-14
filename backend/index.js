6be0ffeccbdf27b96e848b1a056caeb0eebf7a24
BossKiller16 <matejstyblo@seznam.cz>
2021-04-14 19:50:23 +0200

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,58 @@
-/* const { v4: uuid } = require('uuid'); */
-const cors = require("cors");
-const express = require("express")
-const stripe = require("stripe")(process.env.REACT_APP_KEY)
-const app = express();
+const { v4: uuid } = require('uuid')
+const cors = require('cors')
+const express = require('express')
+const Stripe = require('stripe')
+require('dotenv').config()
+
+var bodyParser = require('body-parser')
+const stripe = Stripe(process.env.REACT_APP_KEY)
+const app = express()
 app.use(express.json())
-app.use(cors());
-app.get("/", (req, res) => {
-    res.send("IT WORKS")
-})
-app.get("/payment", (req, res) => {
-    const { cart, token } = req.body.stripeToken
-    console.log("cart", cart);
-    console.log("price", cart.price);
-    /*     const idempontencyKey = uuid(); */
-    return stripe.customers.create({
-        email: token.email,
-        source: token.id
-    }).then(customer => {
-        stripe.charges.create({
-            amount: cart.price * 100,
-            currency: 'czk',
-            customer: customer.id,
-            receipt_email: token.email,
-            source: token,
-            description: "HEJ"
-        })
-    }).then(result => res.status(200).json(result))
-        .catch(err => console.log(err))
-})
-app.listen(8282, () => console.log("LISTENING AT PORT 8282"));
 
+app.use(cors())
+app.get('/', (req, res) => {
+   res.send('IT WORKS')
+})
 
+app.post('/payment', async (req, res) => {
+   const { cart, token, totalSum, billingDetails, email, nameRef } = req.body
+
+   const idempontencyKey = uuid()
+   const customer = await stripe.customers
+      .create({
+         email: token.token.email,
+         shipping: {
+            address: {
+               line1: token.token.card.address_line1,
+               city: token.token.card.address_city,
+               state: 'Czech Republic',
+            },
+            name: token.token.card.name,
+            phone: token.token.card.phone,
+         },
+      })
+      .catch((e) => console.log('customer', e))
+   const charge = await stripe.charges
+      .create({
+         source: token.token.id,
+         amount: totalSum * 100,
+         currency: 'czk',
+         description: 'Potraviny',
+      })
+      .catch((e) => console.log('charge', e))
+
+   const paymentIntent = await stripe.paymentIntents
+      .create({
+         amount: charge.amount,
+         currency: 'czk',
+         payment_method_types: token.card,
+         receipt_email: customer.email,
+      })
+      .catch((e) => console.log('paymentIntent', e))
+   await stripe.paymentIntents.confirm(paymentIntent.id, {
+      payment_method: 'pm_card_visa',
+   })
+
+   return res.send(req.body)
+})
+app.listen(4000, () => console.log('LISTENING AT PORT 4000'))
